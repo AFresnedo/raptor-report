64fa8980406b470d36d9b442a2d003b3f88f538b
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-23 14:07:27 -0700

    fix signup save bugs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,16 @@
 var express = require('express');
 
+var db = require('../models');
+
 var router = express.Router();
+
 
 router.get('/login', function(req, res) {
   res.render('auth/login');
 });
 
 router.post('/login', function(req, res) {
+  console.log(req.body);
   res.send('login post route');
 });
 
@@ -15,7 +19,24 @@ router.get('/signup', function(req, res) {
 });
 
 router.post('/signup', function(req, res) {
-  res.send('temp post signup');
+  console.log(req.body);
+  // defaults is important in following code, adds the input to db
+  db.user.findOrCreate({
+    where: { email: req.body.email },
+    defaults: req.body
+  }).spread(function(user, wasCreated) {
+    if (wasCreated) {
+      // TODO automatically log user in
+      res.redirect('/profile');
+    }
+    else {
+      // TODO give user already-created message
+      res.redirect('/auth/login');
+    }
+  }).catch(function(err) {
+    console.log(err);
+    res.send('error signing up');
+  });
 });
 
 router.get('/logout', function(req, res) {
--- models/user.js
+++ models/user.js
@@ -1,14 +1,46 @@
 'use strict';
+var bcrypt = require('bcrypt');
 module.exports = (sequelize, DataTypes) => {
   var user = sequelize.define('user', {
     firstname: DataTypes.STRING,
     lastname: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Email must be a valid format.'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 16],
+          msg: 'Password must be between 8 and 16 characters long.'
+        }
+      }
+    },
     dob: DataTypes.DATE,
     admin: DataTypes.BOOLEAN,
-    image: DataTypes.STRING
-  }, {});
+    image: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: {
+          msg: 'Not a valid img link.'
+        }
+      }
+    }
+  }, {
+    hooks: {
+      beforeCreate: function(pendingUser) {
+        if (pendingUser && pendingUser.password) {
+          var hash = bcrypt.hashSync(pendingUser.password, 10);
+          pendingUser.password = hash;
+        }
+      }
+    }
+  });
   user.associate = function(models) {
     // associations can be defined here
   };
